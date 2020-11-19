2488afccf75efe6a34e7bef87fd0461683ead826
Mahamurahti <dagility1@gmail.com>
2020-11-19 10:26:51 +0200

    Added Main page

---
+++ fullstack_project/src/components/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <h1>MyMovie</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam amet animi debitis doloremque esse,
+      et nam officia officiis omnis quas quis, rem vitae? Ab, accusantium aperiam, autem eligendi exercitationem facilis
+      impedit inventore itaque minima modi neque officia optio praesentium quia rem sint sit ullam voluptatum? Adipisci
+      corporis exercitationem odio optio quas temporibus? Iusto magnam maxime nobis quam quisquam? Eligendi id nam
+      numquam? Animi atque commodi consectetur cupiditate debitis ducimus ea et, eum expedita facilis illum impedit iste
+      itaque magnam molestias nam necessitatibus nesciunt officia officiis placeat, porro provident quam quasi ratione
+      reiciendis rem repellat repellendus repudiandae sequi sit totam veniam vero voluptatibus? Ab doloremque earum
+      harum nulla quis! Accusantium animi commodi culpa deleniti dolore enim eveniet ex facere ipsa labore, maxime
+      molestiae nemo nihil nobis perferendis praesentium quod ratione recusandae reiciendis velit? Beatae cumque eaque
+      est facilis, fuga iste natus perferendis recusandae, rem sed temporibus voluptate? Accusantium aspernatur atque
+      blanditiis cupiditate esse est explicabo facilis fuga harum ipsa iusto, laboriosam magni mollitia necessitatibus
+      neque, quas quasi quisquam quo quos repellendus sit, soluta tempore. Consequatur cum ea exercitationem fugit, id
+      molestias voluptatum. A adipisci alias aliquid aperiam corporis dignissimos dolor dolore eum expedita facere fuga,
+      impedit ipsa iste iure laborum molestiae necessitatibus, nostrum numquam odit, officia quam quibusdam ratione
+      saepe suscipit tempora tempore voluptatem voluptatum? Ea et exercitationem in ipsam nam nihil officiis ratione ut
+      voluptatem! Accusantium architecto consectetur, cupiditate deleniti enim eveniet molestiae odio odit, quibusdam
+      quidem soluta voluptate, voluptatibus! Accusamus aliquid blanditiis esse id maxime molestias nam quam quod, rerum
+      tenetur. Corporis error est inventore maiores modi non odio? Blanditiis eligendi eum explicabo harum inventore
+      libero, nihil, provident quaerat quidem, rerum sequi velit vero. Fuga illo in molestiae nam nihil, sequi ut.
+      Accusamus accusantium aliquid animi aspernatur aut cumque, distinctio eaque, enim est ex exercitationem hic
+      incidunt ipsam iure laudantium necessitatibus officia porro sint vel voluptas! A ab, aliquam animi aperiam at
+      commodi consequuntur dolor ducimus eius error ex expedita explicabo hic illo labore laborum maxime nam nisi
+      nostrum nulla omnis quae quia quis quo quos saepe sed similique sunt tenetur totam veritatis voluptatem
+      voluptatibus voluptatum? Dolorem esse nostrum rem tempore! A accusamus, accusantium, aspernatur atque aut commodi
+      debitis dicta doloribus est fuga, magnam necessitatibus nisi nostrum numquam quibusdam sint sit temporibus
+      voluptates. Aliquam eius facere ipsa obcaecati pariatur quia reiciendis repellat rerum ut. Adipisci cum dicta
+      dolores eaque enim et fugiat harum hic illo inventore laboriosam libero molestiae mollitia nobis, non nulla
+      numquam odio possimus provident quae quisquam repellat rerum, saepe temporibus totam, vero voluptatum. Aspernatur
+      deleniti doloremque eum illo labore nesciunt non quibusdam totam velit! Aperiam asperiores aut corporis delectus
+      doloribus error esse eveniet magnam minus neque, nihil similique, velit voluptas. Animi autem doloremque
+      laudantium mollitia omnis quaerat quia quo ullam unde! Accusantium, at blanditiis consequuntur deserunt eligendi
+      facilis fugiat in iure labore magnam nam necessitatibus nemo nihil nisi non nulla odio officia, omnis optio porro
+      quod recusandae rem rerum similique sint suscipit vero voluptate? Dignissimos dolore impedit laborum nam nemo
+      nobis, perspiciatis praesentium quas voluptatem!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+}
+</script>
+
+<style scoped>
+
+</style>
