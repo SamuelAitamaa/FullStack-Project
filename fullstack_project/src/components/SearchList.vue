4f970d30a3ce3541d9b98f3305e2628ac7d51ef3
Mahamurahti <dagility1@gmail.com>
2020-11-23 13:47:27 +0200

    Displaying search results is dynamic
    Removed "Search" component, since it's not needed anymore. Search happens dynamically, meaning the is no hardcoded li elements and if there is only one result, only will be displayed and if there 10 results, 10 will be displayed.

--- fullstack_project/src/components/SearchList.vue
+++ fullstack_project/src/components/SearchList.vue
@@ -1,32 +1,46 @@
 <template>
   <div class="searchList">
     <ul>
-      <li><Search index=0 :query=this.input /></li>
-      <li><Search index=1 :query=this.input /></li>
-      <li><Search index=2 :query=this.input /></li>
-      <li><Search index=3 :query=this.input /></li>
-      <li><Search index=4 :query=this.input /></li>
-      <li><Search index=5 :query=this.input /></li>
-      <li><Search index=6 :query=this.input /></li>
-      <li><Search index=7 :query=this.input /></li>
-      <li><Search index=8 :query=this.input /></li>
-      <li><Search index=9 :query=this.input /></li>
-      <li><Search index=10 :query=this.input /></li>
-      <li><Search index=11 :query=this.input /></li>
+      <li v-for="element in this.elements" :key="element.id">
+        <div class="search">
+          <img v-bind:src="element.poster_path" alt="Placeholder image" />
+          <div class="text">
+            <h2>{{ element.title }}</h2>
+          </div>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import Search from "@/components/Search";
+import axios from 'axios';
 
 export default {
   name: "SearchList",
-  components: {
-    Search
+  data(){
+    return {
+      elements: []
+    }
   },
   props: {
     input: String
+  },
+  watch: {
+    input: function() {
+      axios
+      .get(`http://api.themoviedb.org/3/search/movie?query=${this.input}&api_key=7a1108dafa3ea1ef83a43e999a63f38b`)
+      .then(res => {
+        this.elements = res.data.results;
+        this.elements.reduceRight(function (acc, item, index, object) {
+          if(item.poster_path === null || item.poster_path === undefined){
+            object.splice(index, 1);
+          }else{
+            item.poster_path = `http://image.tmdb.org/t/p/w300/${item.poster_path}`;
+          }
+        }, []);
+      });
+    }
   }
 }
 </script>
@@ -41,5 +55,32 @@ ul li{
   list-style-type: none;
   padding: 20px;
   user-select: none;
+}
+
+.search{
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 375px;
+  position: relative;
+}
+.text{
+  display: none;
+
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+
+  text-align: center;
+  color: white;
+  background: rgba(69, 65, 65, .5);
+}
+h2{
+  padding: 5px 10px;
+}
+img{
+  width: 100%;
+  height: 100%;
 }
 </style>
