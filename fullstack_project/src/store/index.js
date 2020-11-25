6a1bf457ce7ae7d569c9618dcccd8fdfd3741bce
SamuelAitamaa <56270085+SamuelAitamaa@users.noreply.github.com>
2020-11-26 00:36:04 +0200

    PersistedState

--- fullstack_project/src/store/index.js
+++ fullstack_project/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -8,6 +9,7 @@ export default new Vuex.Store({
         movies: [],
         loggedIn: true
     },
+    plugins: [createPersistedState()],
     getters: {},
     mutations: {
         newId(state, payload) {
@@ -23,7 +25,6 @@ export default new Vuex.Store({
             });
             state.movies.splice(index, 1)
         }
-
     },
     actions: {}
 });
